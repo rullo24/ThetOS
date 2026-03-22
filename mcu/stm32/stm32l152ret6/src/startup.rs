d4b0db953bd25d74eb16b4fb110d1ad58eff992a
rullo24 <tbd>
2026-03-22 09:17:32 +0800

    [feat] - definitive types given in startup.rs

--- mcu/stm32/stm32l152ret6/src/startup.rs
+++ mcu/stm32/stm32l152ret6/src/startup.rs
@@ -46,9 +46,9 @@ pub extern "C" fn Reset() -> ! {
 
 // copy the data from flash to RAM
 unsafe fn copy_data() {
-    let mut dst = addr_of_mut!(__sdata); // start of data section in RAM
-    let dst_end = addr_of_mut!(__edata); // end of data section in RAM
-    let mut src = addr_of!(__sidata); // start of data section in FLASH
+    let mut dst: *mut u8 = addr_of_mut!(__sdata); // start of data section in RAM
+    let dst_end: *mut u8 = addr_of_mut!(__edata); // end of data section in RAM
+    let mut src: *const u8 = addr_of!(__sidata); // start of data section in FLASH
     while (dst as usize) < (dst_end as usize) { // iterate over each RAM byte
         write_volatile(dst, read_volatile(src)); // write the byte from FLASH to RAM
         dst = dst.wrapping_add(1); // increment the RAM ptr
@@ -58,8 +58,8 @@ unsafe fn copy_data() {
 
 // zero the bss sections
 unsafe fn zero_bss() {
-    let mut p_bss_curr = addr_of_mut!(__sbss); // start of bss section in RAM
-    let end_bss = addr_of_mut!(__ebss); // end of bss section in RAM
+    let mut p_bss_curr: *mut u8 = addr_of_mut!(__sbss); // start of bss section in RAM
+    let end_bss: *mut u8 = addr_of_mut!(__ebss); // end of bss section in RAM
     while (p_bss_curr as usize) < (end_bss as usize) { // iterate over each bss byte
         write_volatile(p_bss_curr, 0); // write 0x0 to current byte
         p_bss_curr = p_bss_curr.wrapping_add(1); // increment ptr
