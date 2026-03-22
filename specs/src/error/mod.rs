087291eb5719321b940efbae08570354bdec5a32
rullo24 <tbd>
2026-03-22 14:04:59 +0800

    [feat] - specs/error/mod.rs implemented

--- specs/src/error/mod.rs
+++ specs/src/error/mod.rs
@@ -1,2 +1,23 @@
-#![no_std]
+use core::fmt::{Display, Formatter, Result as FmtResult};
 
+pub enum ThetosError {
+    InvalidState,
+    Busy,
+    InvalidConfig,
+    Unsupported,
+} 
+
+/// implement Display to print the error message
+impl Display for ThetosError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            ThetosError::InvalidState => f.write_str("invalid state"),
+            ThetosError::Busy => f.write_str("busy"),
+            ThetosError::InvalidConfig => f.write_str("invalid config"),
+            ThetosError::Unsupported => f.write_str("unsupported"),
+        }
+    }
+}
+
+/// alias for the result type
+pub type Result<T> = core::result::Result<T, ThetosError>;
