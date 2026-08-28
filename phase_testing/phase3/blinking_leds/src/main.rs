0a54db36593f37781ddfe5af42eb349839d8e653
Connor Rullo <crullo@vektis.com.au>
2026-08-28 10:01:03 +0800

    [feat] - added GPIO specs

--- phase_testing/phase3/blinking_leds/src/main.rs
+++ phase_testing/phase3/blinking_leds/src/main.rs
@@ -2,8 +2,11 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::ptr::{addr_of_mut, null_mut};
 use entry::entry;
 use nucleo_l152re::System;
+use specs::common::TaskId;
+use specs::kernel::TaskPriority;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,10 +15,28 @@ fn panic(_info: &PanicInfo) -> ! {
 
 static mut STACK_POOL: [u8; 4096] = [0; 4096];
 
+extern "C" fn blink_onboard(_arg: *mut ()) -> ! {
+    loop {} // TODO: replace with blink logic
+}
+
 #[entry]
 fn app_main() -> ! {
     let p_stack_pool = unsafe { &mut *addr_of_mut!(STACK_POOL) };
     let system = System::new_with_pool(p_stack_pool);
 
     // OUTPUT direction on LED GPIO
+
+    // define tasks
+    system
+        .spawn_task(
+            TaskId(1),
+            TaskPriority::default(),
+            4096,
+            blink_onboard,
+            null_mut(),
+        )
+        .unwrap();
+
+    // start scheduler
+    system.run();
 }
--- specs/src/bsp/gpio.rs
+++ specs/src/bsp/gpio.rs
@@ -1,16 +1,70 @@
 use core::result::Result;
 
-/// board-facing GPIO resource contract exposed to application code.
-pub trait BoardGpioPin {
-    type Error: core::fmt::Debug;
-
-    fn set_level(&mut self, level: GpioLevel) -> Result<(), Self::Error>;
-    fn read_level(&self) -> Result<GpioLevel, Self::Error>;
-    fn toggle(&mut self) -> Result<(), Self::Error>;
+// used for local typestate
+mod sealed {
+    pub trait Sealed {}
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+// typestate marke
+pub trait PinMode: sealed::Sealed {}
+
+// direction markers
+pub struct Uninit; // no direction (pre-config state)
+pub struct Input;
+pub struct Output;
+
+impl sealed::Sealed for Uninit {}
+impl sealed::Sealed for Input {}
+impl sealed::Sealed for Output {}
+impl PinMode for Uninit {}
+impl PinMode for Input {}
+impl PinMode for Output {}
+
+// logic level of a GPIO line
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum GpioLevel {
     Low,
     High,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum PullState {
+    HighZ, // floating
+    PullUp,
+    PullDown,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum OutputStyle {
+    PushPull,  // active drive HIGH or LOW
+    OpenDrain, // active drive LOW (relies on pull-up for HIGH)
+}
+
+pub trait UninitPin {
+    type Input: InputPin;
+    type Output: OutputPin;
+
+    /// DESCRIPTION
+    /// Configures the pin in INPUT direction
+    fn into_input(self, pull: PullState) -> Self::Input;
+
+    /// DESCRIPTION
+    /// Configures the pin in OUTPUT direction
+    fn into_output(self, style: OutputStyle) -> Self::Output;
+}
+
+pub trait InputPin {
+    type Error: core::fmt::Debug;
+
+    /// DESCRIPTION
+    /// Reads the current logical level of the pin and returns it
+    fn read(&self) -> Result<GpioLevel, Self::Error>;
+}
+
+pub trait OutputPin {
+    type Error: core::fmt::Debug;
+
+    /// DESCRIPTION
+    /// Sets the logical level of the pin to a GpioLevel
+    fn set(&mut self, level: GpioLevel) -> Result<(), Self::Error>;
 }
